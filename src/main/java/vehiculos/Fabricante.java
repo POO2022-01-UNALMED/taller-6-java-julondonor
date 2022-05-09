199b77b823f9a0c0b8d94f5a8cb54d1dd83ea883
Julián E. Londoño <julondonor@unal.edu.co>
2022-05-09 16:45:16 -0500

    Edited the Fabricante and Pais classes

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,11 +1,16 @@
 package vehiculos;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Fabricante {
 	private String nombre;
 	private Pais pais;
+	public static ArrayList<Fabricante> Fabricantes = new ArrayList<Fabricante>();
 	
+	public Fabricante() {}
 	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre; this.pais = pais;
+		Fabricante.Fabricantes.add(this);
 	}
 	public String getNombre() {
 		return this.nombre;
@@ -20,4 +25,22 @@ public class Fabricante {
 	public void setPais(Pais pais) {
 		this.pais = pais;
 	}
+	
+	public static Fabricante fabricaMayorVentas() {
+		Hashtable<String, Integer> freq = new Hashtable<String,Integer>();
+		int maximo = 0;
+		Fabricante ganador = new Fabricante();
+		for(Fabricante fabricante: Fabricante.Fabricantes) {
+			String nombre= fabricante.getNombre();
+			Integer cuantas = freq.get(nombre);
+			freq.put(nombre, (cuantas == null) ? 1 : cuantas+1);
+			
+			if (((cuantas == null) ? 1 : cuantas+1) >maximo) {
+				maximo = cuantas+1;
+				ganador = fabricante;
+			}
+		}
+		
+		return ganador;
+		}
 }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,11 +1,40 @@
 package vehiculos;
+import java.util.*;
+
 
 public class Pais {
 	private String nombre;
+	private static ArrayList<Pais> Paises = new ArrayList<Pais>();
+	
+	public Pais() {}
+	public Pais(String nombre) {
+		this.nombre = nombre;
+		Pais.Paises.add(this);
+	}
 	public String getNombre() {
 		return this.nombre;
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	public static Pais paisMasVendedor() {
+		Hashtable<String, Integer> freq = new Hashtable<String,Integer>();
+		int maximo = 0;
+		Pais ganador = new Pais();
+		for(Pais pais: Pais.Paises) {
+			String nombre= pais.getNombre();
+			Integer cuantas = freq.get(nombre);
+			freq.put(nombre, (cuantas == null) ? 1 : cuantas+1);
+			
+			if (((cuantas == null) ? 1 : cuantas+1) >maximo) {
+				maximo = cuantas+1;
+				ganador = pais;
+			}
+		}
+		
+		return ganador;
+		}
+		
 }
+
